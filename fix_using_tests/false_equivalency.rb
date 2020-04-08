c5e63d03d6da2ffbbcd0b15e53f1560fefd266b6
Steven Ross <ross.steven@me.com>
2020-04-08 19:47:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,12 +11,13 @@ def prompt_user
 end
 
 def selection(num)
+  num = 2
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
      "HAM HAM HAM IN MY TUMMY"
   end
-  binding.pry 
+  binding.pry
 end
 
 def runner
